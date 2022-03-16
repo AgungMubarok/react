83b8c9937171dca5711f64e5ab91b74025dfaf14
AgungMubarok <agungmubarok555@gmail.com>
2022-03-16 18:19:44 +0700

    state

--- src/screen/landing-page/landing-page.jsx
+++ src/screen/landing-page/landing-page.jsx
@@ -1,25 +1,27 @@
-import { Link } from 'react-router-dom';
-import { Avatar } from '../../components/avatar';
-import { Card } from '../../components/card';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 import Layout from '../../layout';
 import './landing-page.css';
 
 function AppKu() {
+  const [nama, setNama] = useState('yudi');
+  const [button, setButton] = useState('ubah nama');
+  console.log(nama);
+  function onClickButton() {
+    if (nama === 'rizki') {
+      setNama('yudi');
+      setButton('ubah nama');
+    } else {
+      setNama('rizki');
+      setButton('kembali');
+    }
+  }
   return (
     <Layout>
-      <Avatar imageAvatar="https://images.unsplash.com/photo-1644982652063-1a9b8d24f0f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-      <div className="layouting">
-        <Card
-          textCard="html"
-          avatarPicture="https://images.unsplash.com/photo-1644982652063-1a9b8d24f0f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-          locationText="right"
-        />
-        <Card
-          textCard="css"
-          avatarPicture="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        />
-      </div>
-      <Link to="/about">about</Link>
+      <Container>
+        <div>ini nama yang tampil : {nama}</div>
+        <button onClick={onClickButton}>{button}</button>
+      </Container>
     </Layout>
   );
 }
